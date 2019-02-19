d24ba13f4b1cd00b9c00a0c22ea1c2e911c1f4a8
Shahriar <shahriarkamal.dsi@gmail.com>
2019-02-19 13:21:52 +0600

    work partial for jMapping

--- src/main/java/test/keywordScripts/UITable.java
+++ src/main/java/test/keywordScripts/UITable.java
@@ -86,12 +86,15 @@ public class UITable extends  UtilKeywordScript{
             List<Map> tableData = new ArrayList<Map>();
             WebElement rootElement = WebObjectSearch.getWebElement(webDriver,objectLocatorData);
             List<WebElement> tables  = rootElement.findElements(By.tagName("table"));
+            boolean specialTable = false;
             if(tables.size() <2) {
                 return getSingleRowfromSingleTable(objectLocatorData,columnName,columnValue,rowIndex);
             }
             WebElement head = tables.get(0) ;
             WebElement body = tables.get(1) ;
             List<WebElement> rows = head.findElements(By.tagName("tr"));
+            if(rows.size()>1)
+                specialTable = true;
             List<WebElement> headCells = rows.get(0).findElements(By.tagName("th"));
             /*
             for(WebElement row : rows) {
@@ -114,7 +117,13 @@ public class UITable extends  UtilKeywordScript{
                             }
                         }
                     }
-                    String key =  String.valueOf(index) + "," + headCells.get(index).getText();
+                    String key = "";
+                    if(specialTable) {
+                      //  String clmnName = bodyCell.getAttribute("title") == "" ? "na" : bodyCell.getAttribute("title") ;
+                        key = String.valueOf(index) + "," + bodyCell.getAttribute("title") ;
+                    }
+                     else
+                         key =  String.valueOf(index) + "," + headCells.get(index).getText();
                     rowdata.put(key,bodyCell);
                     index++;
 
@@ -299,7 +308,7 @@ public class UITable extends  UtilKeywordScript{
              rowIndex = testData.split(",")[1] ;
             columnValue = testData.split(",")[2] ;
             Map<String, WebElement> row = getSingleRowfromTable(objectLocatorData,null,null,Integer.valueOf(rowIndex));
-            System.out.println(row);
+            //System.out.println(row);
             if(null == row || row.isEmpty())
                 return new LogMessage(false, "no table data");
             for (String key : row.keySet()) {
@@ -329,24 +338,32 @@ public class UITable extends  UtilKeywordScript{
             String columnName = "" ;
             String columnValue = "" ;
             String rowIndex =  "" ;
+            String inputTag = "";
             if(!validateTestData(testData,3))
                 return new LogMessage(false, "test data not valid");
             columnName = testData.split(",")[0] ;
             rowIndex = testData.split(",")[1] ;
             columnValue = testData.split(",")[2] ;
+            if(testData.split(",").length > 3)
+                inputTag = testData.split(",")[3] ;
+
             Map<String, WebElement> row = getSingleRowfromTable(objectLocatorData,null,null,Integer.valueOf(rowIndex));
-            System.out.println(row);
+            //System.out.println(row);
             if(null == row || row.isEmpty())
                 return new LogMessage(false, "no table data");
             for (String key : row.keySet()) {
+                if(key.split(",").length <2) continue;
                 String clName = key.split(",")[1];
                 if(columnName.equals(clName)){
                     WebElement element = row.get(key) ;
                     if(element.isEnabled()) {
-                        element.click();
+                        if(inputTag != "")
+                            element.findElement(By.tagName(inputTag)).click();
+                        else
+                            element.click();
                         // UtilKeywordScript.delay(3);
                        // element.sendKeys(columnValue);
-                        return new LogMessage(true,"enter text data");
+                        return new LogMessage(true,"click table data");
                     } else {
                         return new LogMessage(false," text field disabled");
                     }
@@ -355,7 +372,59 @@ public class UITable extends  UtilKeywordScript{
             return new LogMessage(true, "proper cell is not present.");
 
         } catch ( Exception ex ) {
+            ex.printStackTrace();
             return new LogMessage(false, "exception occured " + ex.getMessage());
+        }
+    }
+
+    public LogMessage filterTableByColumn(String objectLocatorData,String testData) {
+        try {
+            String columnName = "" ;
+            String columnValue = "" ;
+            if(!validateTestData(testData,2))
+                return new LogMessage(false, "test data not valid");
+            columnName = testData.split(",")[0] ;
+            columnValue = testData.split(",")[1] ;
+
+                List<WebElement> headers = getCoumnWebElements(objectLocatorData);
+                for(WebElement element : headers) {
+                    if(element.getText().equals(columnName)) {
+                        UIBase uiBase = new UIBase(webDriver);
+                        uiBase.Click(element.findElement(By.xpath("//*[@class='k-icon k-filter']")));
+                       // element.findElement(By.xpath("//*[@class='k-icon k-filter']")).click();
+                        UtilKeywordScript.delay(1);
+                        webDriver.findElement(By.xpath("//input[@class='k-textbox']")).sendKeys(columnValue);   //button[text()='Filter']
+                        UtilKeywordScript.delay(1);
+                        webDriver.findElement(By.xpath("//button[text()='Filter']")).click();
+                        UtilKeywordScript.delay(3);
+                       return  new LogMessage(true, "column filter done");
+
+                    }
+                }
+            return  new LogMessage(false, "column name not present");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new LogMessage(false,"exception occured " + ex.getMessage());
+        }
+    }
+
+    private List<WebElement> getCoumnWebElements(String objectLocatorData) {
+        try {
+            List<Map> tableData = new ArrayList<Map>();
+            WebElement rootElement = WebObjectSearch.getWebElement(webDriver,objectLocatorData);
+            List<WebElement> tables  = rootElement.findElements(By.tagName("table"));
+            if(tables.size() <2) {
+                // plz handle for single table
+                return new ArrayList<WebElement>();
+              //  return getSingleRowfromSingleTable(objectLocatorData,columnName,columnValue,rowIndex);
+            }
+            WebElement head = tables.get(0) ;
+            List<WebElement> rows = head.findElements(By.tagName("tr"));
+            return rows.get(0).findElements(By.tagName("th"));
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ArrayList<WebElement>();
         }
     }
 
