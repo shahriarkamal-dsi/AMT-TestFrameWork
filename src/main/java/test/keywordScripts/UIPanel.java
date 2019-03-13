65f6e6fe78134247f1795ad3e075261ac3c715b3
ehsanulhaque.dsi <ehsanulhaque.dsi@gmail.com>
2019-03-13 16:21:48 +0600

    Resolve conflict

--- src/main/java/test/keywordScripts/UIPanel.java
+++ src/main/java/test/keywordScripts/UIPanel.java
@@ -1,4 +1,39 @@
 package test.keywordScripts;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import test.Log.LogMessage;
+import test.objectLocator.WebObjectSearch;
+
 public class UIPanel {
+
+    private WebDriver webDriver;
+
+    public UIPanel(WebDriver driver) {
+        this.webDriver = driver ;
+    }
+
+    public UIPanel(){
+
+    }
+
+    public LogMessage VerifyPanelContentTrue(String objectLocatorData,String testData){
+        try{
+            String elementPath = "//*[text()='" + testData + "']";
+            WebElement rootPanel = WebObjectSearch.getWebElement(webDriver,objectLocatorData);
+            if (null == rootPanel)
+                return new LogMessage(false,"Panel no found");
+
+            WebElement elementToFind = rootPanel.findElement(By.xpath(elementPath));
+            if (null == elementToFind)
+                return new LogMessage(false,"Panel content not found");
+
+            return new LogMessage(true, "Panel content verified");
+        }catch (Exception e){
+            e.printStackTrace();
+            return new LogMessage(false, "Exception occur " +e.getMessage());
+        }
+    }
+
 }
