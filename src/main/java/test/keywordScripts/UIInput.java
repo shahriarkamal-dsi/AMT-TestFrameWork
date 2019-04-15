40804777303486615c7da9f92824c9e5352b9a3b
ehsanulhaque.dsi <ehsanulhaque.dsi@gmail.com>
2019-04-15 18:36:25 +0600

    Resolve conflict

---
+++ src/main/java/test/keywordScripts/UIInput.java
@@ -0,0 +1,56 @@
+package test.keywordScripts;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import test.Log.LogMessage;
+import test.objectLocator.WebObjectSearch;
+import test.utility.PropertyConfig;
+
+public class UIInput {
+    private WebDriver webDriver;
+
+    public UIInput(WebDriver driver) {
+        this.webDriver = driver ;
+    }
+    public LogMessage compareValue(String objectLocator, String testData){
+        try{
+            String[] splittedTestData=testData.split(",");
+            String attribute = getValue(objectLocator);
+            if(attribute.equals(splittedTestData[0]))
+                return new LogMessage(true, "Value is verified");
+            else
+                return new LogMessage(false, "Value is not verified");
+        }catch (Exception e){
+            return new LogMessage(false,"Exception occur" + e.getMessage());
+        }
+    }
+    public String getValue(String objectLocator){
+        try {
+            WebElement webElement = WebObjectSearch.getWebElement(webDriver, objectLocator);
+            if (null == webElement) {
+                return null;
+            }
+            return webElement.getAttribute("value");
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    public LogMessage setValue(String objectLocator, String value){
+        try{
+            JavascriptExecutor jse = (JavascriptExecutor)webDriver;
+
+            WebElement element = WebObjectSearch.getWebElement(webDriver,objectLocator);
+            if(null == element )
+                return new LogMessage(false,"webElement is not founding");
+            jse.executeScript("arguments[0].scrollIntoView(true);", element);
+            UtilKeywordScript.delay(PropertyConfig.ONE_SECOND);
+            jse.executeScript("arguments[0].value='" + value + "';",element);
+            return new LogMessage(true,"Value set successfully");
+        }catch (Exception e){
+            return new LogMessage(false,"Exception occur" + e.getMessage());
+        }
+    }
+
+}
