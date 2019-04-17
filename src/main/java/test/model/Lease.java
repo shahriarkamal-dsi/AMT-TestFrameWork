c7a20682f2e04b6b87696f62e8616ddf031c04d5
ehsanulhaque.dsi <ehsanulhaque.dsi@gmail.com>
2019-04-17 13:40:43 +0600

    Add: getter setter and Map for Lease,Property and Space in model

--- src/main/java/test/model/Lease.java
+++ src/main/java/test/model/Lease.java
@@ -1,6 +1,8 @@
 package test.model;
 
 import javax.persistence.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Entity
 public class Lease {
@@ -8,9 +10,30 @@ public class Lease {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     @Column(unique = true)
-    private String leaseNmae;
+    private String leaseName;
     @Column(unique = true)
     private String leaseCode;
+
+    @Column(name = "property_name", length = 30)
+    private String propertyName;
+    @Column(name = "property_code", length = 30)
+    private String propertyCode;
+    @Column(name = "lease_status", length = 30)
+    private String leaseStatus;
+    @Column(name = "lease_type", length = 30)
+    private String leaseType;
+    @Column(name = "billing_type", length = 30)
+    private String billingType;
+    @Column(name = "begin_date")
+    private String beginDate;
+    @Column(name = "expiration_date")
+    private String expirationDate;
+    @Column(name = "lease_group1", length = 30)
+    private String leaseGroup1;
+    @Column(name = "code_type", length = 30)
+    private String codeType;
+    @Column(name = "contract_type", length = 30)
+    private String contractType;
 
     public long getId() {
         return id;
@@ -20,12 +43,12 @@ public class Lease {
         this.id = id;
     }
 
-    public String getLeaseNmae() {
-        return leaseNmae;
+    public String getLeaseName() {
+        return leaseName;
     }
 
-    public void setLeaseNmae(String leaseNmae) {
-        this.leaseNmae = leaseNmae;
+    public void setLeaseName(String leaseNmae) {
+        this.leaseName = leaseNmae;
     }
 
     public String getLeaseCode() {
@@ -34,5 +57,106 @@ public class Lease {
 
     public void setLeaseCode(String leaseCode) {
         this.leaseCode = leaseCode;
+    }
+
+
+    public String getPropertyName() {
+        return propertyName;
+    }
+
+    public void setPropertyName(String propertyName) {
+        this.propertyName = propertyName;
+    }
+
+    public String getPropertyCode() {
+        return propertyCode;
+    }
+
+    public void setPropertyCode(String propertyCode) {
+        this.propertyCode = propertyCode;
+    }
+
+    public String getLeaseStatus() {
+        return leaseStatus;
+    }
+
+    public void setLeaseStatus(String leaseStatus) {
+        this.leaseStatus = leaseStatus;
+    }
+
+    public String getLeaseType() {
+        return leaseType;
+    }
+
+    public void setLeaseType(String leaseType) {
+        this.leaseType = leaseType;
+    }
+
+    public String getBillingType() {
+        return billingType;
+    }
+
+    public void setBillingType(String billingType) {
+        this.billingType = billingType;
+    }
+
+    public String getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(String beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getLeaseGroup1() {
+        return leaseGroup1;
+    }
+
+    public void setLeaseGroup1(String leaseGroup1) {
+        this.leaseGroup1 = leaseGroup1;
+    }
+
+    public String getCodeType() {
+        return codeType;
+    }
+
+    public void setCodeType(String codeType) {
+        this.codeType = codeType;
+    }
+
+    public String getContractType() {
+        return contractType;
+    }
+
+    public void setContractType(String contractType) {
+        this.contractType = contractType;
+    }
+
+    public Map<String, String> getLeaseMap(){
+
+        Map<String,String> leaseMap = new HashMap<String, String>();
+
+        leaseMap.put("propertyName",propertyName);
+        leaseMap.put("propertyCode",propertyCode);
+        leaseMap.put("dbaName",leaseName);
+        leaseMap.put("leaseCode",leaseCode);
+        leaseMap.put("leaseStatus",leaseStatus);
+        leaseMap.put("leaseType",leaseType);
+        leaseMap.put("billingType",billingType);
+        leaseMap.put("beginDate",beginDate);
+        leaseMap.put("expirationDate",expirationDate);
+        leaseMap.put("leaseGroup1",leaseGroup1);
+        leaseMap.put("codeType",codeType);
+        leaseMap.put("contractType",contractType);
+
+        return  leaseMap;
     }
 }
--- src/main/java/test/model/Property.java
+++ src/main/java/test/model/Property.java
@@ -1,6 +1,8 @@
 package test.model;
 
 import javax.persistence.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Entity
 public class Property {
@@ -8,9 +10,40 @@ public class Property {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     @Column(unique = true)
-    private String propertyNmae;
+    private String propertyName;
     @Column(unique = true)
     private String propertyCode;
+
+    @Column(name = "address", length = 30)
+    private String address;
+    @Column(name = "country", length = 30)
+    private String country;
+    @Column(name = "state", length = 30)
+    private String state;
+    @Column(name = "postal", length = 30)
+    private String postal;
+    @Column(name = "city", length = 30)
+    private String city;
+    @Column(name = "code_type", length = 30)
+    private String codeType;
+    @Column(name = "status", length = 30)
+    private String status;
+    @Column(name = "currency", length = 30)
+    private String currency;
+    @Column(name = "chart_type", length = 30)
+    private String chartType;
+    @Column(name = "sqFt_rentable", length = 30)
+    private String sqFtRentable;
+    @Column(name = "property_group1", length = 30)
+    private String propertyGroup1;
+    @Column(name = "property_group2", length = 30)
+    private String propertyGroup2;
+    @Column(name = "property_group3", length = 30)
+    private String propertyGroup3;
+    @Column(name = "auto_manage")
+    private String autoManage;
+
+
 
     public Property() {
     }
@@ -23,12 +56,12 @@ public class Property {
         this.id = id;
     }
 
-    public String getPropertyNmae() {
-        return propertyNmae;
+    public String getpropertyName() {
+        return propertyName;
     }
 
-    public void setPropertyNmae(String propertyNmae) {
-        this.propertyNmae = propertyNmae;
+    public void setpropertyName(String propertyNmae) {
+        this.propertyName = propertyNmae;
     }
 
     public String getPropertyCode() {
@@ -37,5 +70,141 @@ public class Property {
 
     public void setPropertyCode(String propertyCode) {
         this.propertyCode = propertyCode;
+    }
+
+
+    public String getPostal() {
+        return postal;
+    }
+
+    public void setPostal(String postal) {
+        this.postal = postal;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address1) {
+        this.address = address1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCodeType() {
+        return codeType;
+    }
+
+    public void setCodeType(String codeType) {
+        this.codeType = codeType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getChartType() {
+        return chartType;
+    }
+
+    public void setChartType(String chartType) {
+        this.chartType = chartType;
+    }
+
+    public String getPropertyGroup1() {
+        return propertyGroup1;
+    }
+
+    public void setPropertyGroup1(String propertyGroup1) {
+        this.propertyGroup1 = propertyGroup1;
+    }
+
+    public String getPropertyGroup2() {
+        return propertyGroup2;
+    }
+
+    public void setPropertyGroup2(String propertyGroup2) {
+        this.propertyGroup2 = propertyGroup2;
+    }
+
+    public String getPropertyGroup3() {
+        return propertyGroup3;
+    }
+
+    public void setPropertyGroup3(String propertyGroup3) {
+        this.propertyGroup3 = propertyGroup3;
+    }
+
+    public String getAutoManage() {
+        return autoManage;
+    }
+
+    public void setAutoManage(String autoManage) {
+        this.autoManage = autoManage;
+    }
+
+    public String getSqFtRentable() {
+        return sqFtRentable;
+    }
+
+    public void setSqFtRentable(String sqFtRentable) {
+        this.sqFtRentable = sqFtRentable;
+    }
+    public Map<String,String > getPropertyMap(){
+
+        Map<String,String> propertyMap=new HashMap<>();
+
+        propertyMap.put("propertyName",propertyName);
+        propertyMap.put("propertyCode",propertyCode);
+        propertyMap.put("address1",address);
+        propertyMap.put("country",country);
+        propertyMap.put("state",state);
+        propertyMap.put("postal",postal);
+        propertyMap.put("city",city);
+        propertyMap.put("codeType",codeType);
+        propertyMap.put("status",status);
+        propertyMap.put("currency",currency);
+        propertyMap.put("chartType",chartType);
+        propertyMap.put("sqFtRentable",sqFtRentable);
+        propertyMap.put("propertyGroup1",propertyGroup1);
+        propertyMap.put("propertyGroup2",propertyGroup2);
+        propertyMap.put("propertyGroup3",propertyGroup3);
+        propertyMap.put("autoManage",autoManage);
+
+        return propertyMap;
     }
 }
