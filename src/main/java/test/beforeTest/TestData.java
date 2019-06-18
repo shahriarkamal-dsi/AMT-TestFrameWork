54fbeacac239265d721bd8724f133dfbf3f9b8dd
Shahriar <shahriarkamal.dsi@gmail.com>
2019-06-18 20:58:52 +0600

    bug fixing

--- src/main/java/test/beforeTest/TestData.java
+++ src/main/java/test/beforeTest/TestData.java
@@ -61,8 +61,8 @@ public class TestData {
             String clientId = TestPlan.getInstance().getCurrentTestEnvironment().getClient();
             String environment = TestPlan.getInstance().getCurrentTestEnvironment().getEnv();
             List<LogMessage> logMessages = new ArrayList<LogMessage>() ;
-            //NotExecutedPreqData notExecutedPreqData =  testDataMapService.getNotExecutedPreqData(testCaseId,clientId,environment,100);
-            NotExecutedPreqData notExecutedPreqData =  new NotExecutedPreqData();
+            NotExecutedPreqData notExecutedPreqData =  testDataMapService.getNotExecutedPreqData(testCaseId,clientId,environment,100);
+           // NotExecutedPreqData notExecutedPreqData =  new NotExecutedPreqData();
             PropertyCreateAndSearch propertyCreateAndSearch = _propertyCreateAndSearch ;
             List<Map> propertyRecords=notExecutedPreqData.getPropertyList().stream().map( e -> e.getPropertyMap()).collect(Collectors.toList());
            for(Map propertyRecord: propertyRecords){
